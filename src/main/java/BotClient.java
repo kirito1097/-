e1a7c1230d8001ec34e792ddeb41b87cbf9e2457
kirito1097 <foxy.top3974@gmail.com>
2026-05-25 20:58:39 +0300

    Fix connection parameters in BotClient

--- src/main/java/BotClient.java
+++ src/main/java/BotClient.java
@@ -1,3 +1,4 @@
+import mindustry.Vars;
 import mindustry.net.*;
 import mindustry.gen.*;
 import arc.*;
@@ -5,17 +6,16 @@ import arc.util.*;
 
 public class BotClient extends mindustry.mod.Plugin {
 
-    String message = "Привет!"; // сюда пишешь своё сообщение
+    String message = "Привет!";
 
     @Override
     public void init() {
         Events.on(mindustry.game.EventType.ClientLoadEvent.class, e -> {
-            // подключаемся к серверу
-            Vars.net.connect("45.61.49.88:5500", 6567, () -> {
+            Vars.net.connect("45.61.49.88", 5500, () -> {
                 Timer.schedule(() -> {
                     Call.sendChatMessage(message);
                 }, 2f);
             });
         });
     }
-  }
+}
